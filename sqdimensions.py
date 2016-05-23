372ce3752e12df4f04c023c7f86d9b6b0ec0bc69
sanjnaverma <sanjnaverma@nyu.edu>
2016-05-23 16:55:20 -0700

    adding more stuff

--- sqdimensions.py
+++ sqdimensions.py
@@ -1,7 +1,14 @@
-var matrix = [
-[0000011100],
-[0011111100],
-[0011111000],
-[1111111100],
-[0110000000]]
+matrix = [
+	["0000011100"],
+	["0011111100"],
+	["0011111000"],
+	["1111111100"],
+	["0110000000"]
+]
 
+#find the largest square dimension of 1's
+max_dim = 0 #this will change
+current_dim = 0
+for line in matrix:
+	for element in line:
+		if(element == 1):
